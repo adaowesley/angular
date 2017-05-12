e093e331e6738c774a88255f0d3a305d92f55a08
adao.wesley@gmail.com <150912>
2017-05-11 23:23:18 -0300

    Removed sensitive information.

--- src/app/client/client-mock.ts
+++ src/app/client/client-mock.ts
@@ -1,6 +1,6 @@
 import { Client } from './client-model'
 export const CLIENTS:Client[]=[
-  {id:1, name: 'Wesley Santos', email:'adao.wesley@gmail.com',telephone: '(00) 9987-9088'},
+  {id:1, name: 'Wesley Santos', email:'me@gmail.com',telephone: '(00) 9987-9088'},
   {id:2, name: 'Paulo Lima', email:'paulolima@yahoo.com',telephone: '(00) 9987-9088'},
   {id:3, name: 'Suzana Ribeiro', email:'suzana@net.com',telephone: '(00) 9987-9088'},
   {id:4, name: 'Paola Fernandes', email:'paolafernand@hotmail.com',telephone: '(00) 9987-9088'},
